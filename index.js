cdea202874b166ef1cf680584d6e482c7e225cd5
MohamedSaber55 <88712250+MohamedSaber55@users.noreply.github.com>
2023-06-27 18:30:34 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express'
 import dbConnection from './databases/dbConnection.js'
 import userRouter from './src/modules/users/users.router.js'
+import cors from 'cors'
 const app = express()
 const port = 3000
 app.use(json())
+app.use(cors())
 app.use('/users', userRouter)
 import dotenv from 'dotenv'
 dotenv.config()
